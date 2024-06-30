f1dc2d19c8fecbcbfdc7badc594c0c035450bd08
mmsmesut <m.mesutsincar@hotmail.com>
2024-06-30 17:27:42 +0300

    Api entegration added.

---
+++ BaseApi.Presentation/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaseApi.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //Calling : "api/Home" -> [Route("api/[controller]")]
+    //Calling : "Home" -> [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet]//https://localhost:7018/api/Home
+        public ActionResult<string> Get()
+        {
+            return "Hello World from Web API!ss";
+        }
+
+        [HttpGet]
+        [Route("Gets")]//https://localhost:7018/api/Home/gets
+        public ActionResult<string> Gets()
+        {
+            return "Hello World from Web API!ss";
+        }
+    }
+}
--- BaseApi.Presentation/Program.cs
+++ BaseApi.Presentation/Program.cs
@@ -1,9 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
+//Add For Api:1
+builder.Services.AddControllers();//ASP.NET Core MVC (Model-View-Controller) altyapýsýný uygulamanýza dahil etmek için kullanýlýr. 
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
+//Add For Api:2
+app.MapControllers(); //ASP.NET Core uygulamasýnda HTTP isteklerini API kontrollerine yönlendirmek için kullanýlan bir yöntemdir
 
 app.Run();
